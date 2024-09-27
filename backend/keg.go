3bf23ea217066b8f400856823650aff1fa6a2621
Tomas Kozak <kozak@talko.cz>
2024-09-27 15:14:57 +0200

    Amend keg weights

--- backend/keg.go
+++ backend/keg.go
@@ -6,13 +6,13 @@ func calcBeersLeft(keg int, weight float64) int {
 	var kegWeight float64 = 0 // ignore keg if not found
 	switch keg {
 	case 10:
-		kegWeight = 5.45
+		kegWeight = 6
 	case 15:
-		kegWeight = 6.15
+		kegWeight = 7
 	case 20:
-		kegWeight = 6.4 // guess
+		kegWeight = 7.25 // guess
 	case 30:
-		kegWeight = 6.8
+		kegWeight = 7.5
 	case 50:
 		kegWeight = 10.1 // guess
 	}
