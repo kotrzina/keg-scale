9880462cb0f02458f4e3d9508ec6836c7fe90ea7
Tomas Kozak <kozak@talko.cz>
2024-10-29 22:03:15 +0100

    Fix spacing

--- frontend/src/Field.js
+++ frontend/src/Field.js
@@ -1,4 +1,4 @@
-import {Toast} from "react-bootstrap";
+import {Col, Toast} from "react-bootstrap";
 
 function Field(props) {
 
@@ -16,27 +16,29 @@ function Field(props) {
     }
 
     return (
-        <Toast style={{margin: "5px"}} hidden={props.hidden}>
-            <Toast.Header closeButton={false}>
-                <strong className="me-auto">
-                    {props.title}&nbsp;&nbsp;
-                    <img
-                        hidden={!props.loading}
-                        src={"/Rhombus.gif"}
-                        width="16"
-                        height="16"
-                        className="align-middle"
-                        alt="Loader"
-                    />
-                </strong>
-                <small>{props.info}</small>
-            </Toast.Header>
-            <Toast.Body>
-                <div className={getBodyClass()}>
-                    {props.children}
-                </div>
-            </Toast.Body>
-        </Toast>
+        <Col xs={12} sm={12} md={6} lg={4} xl={4} xxl={3} hidden={props.hidden} className={"toast-col"}>
+            <Toast>
+                <Toast.Header closeButton={false}>
+                    <strong className="me-auto">
+                        {props.title}&nbsp;&nbsp;
+                        <img
+                            hidden={!props.loading}
+                            src={"/Rhombus.gif"}
+                            width="16"
+                            height="16"
+                            className="align-middle"
+                            alt="Loader"
+                        />
+                    </strong>
+                    <small>{props.info}</small>
+                </Toast.Header>
+                <Toast.Body>
+                    <div className={getBodyClass()}>
+                        {props.children}
+                    </div>
+                </Toast.Body>
+            </Toast>
+        </Col>
     )
 
 }
