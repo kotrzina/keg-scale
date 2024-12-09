5ca55bbcdf033f0ff38f9cb59157c7afc4793720
Tomas Kozak <kozak@talko.cz>
2024-12-09 14:29:04 +0100

    Remove debug

--- backend/pkg/ai/provider_weather.go
+++ backend/pkg/ai/provider_weather.go
@@ -10,7 +10,7 @@ import (
 func ProvideWeather() (string, error) {
 	now := time.Now()
 	url := fmt.Sprintf("https://data.pocasi-data.cz/data/pocasi/v1/aladin/%d/%02d/%02d/06/100/337/dnes.json", now.Year(), int(now.Month()), now.Day())
-	fmt.Println(url)
+
 	resp, err := http.DefaultClient.Get(url)
 	if err != nil {
 		return "", fmt.Errorf("could not get weather data: %w", err)
@@ -23,6 +23,5 @@ func ProvideWeather() (string, error) {
 		return "", fmt.Errorf("could not read response body: %w", err)
 	}
 
-	fmt.Println(string(body))
 	return string(body), nil
 }
