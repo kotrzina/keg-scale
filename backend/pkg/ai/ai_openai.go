f6c75c4dc75cbc9d6ffa91a5cb5898964f01357e
Tomas Kozak <kozak@talko.cz>
2025-06-02 19:42:34 +0200

    Use GPT4_1

--- backend/pkg/ai/ai_openai.go
+++ backend/pkg/ai/ai_openai.go
@@ -45,10 +45,10 @@ func NewOpenAi(ctx context.Context, conf *config.Config, s *scale.Scale, m *prom
 
 func (ai *OpenAi) GetQuality(quality ModelQuality) string {
 	if quality == ModelQualityHigh {
-		return openai.ChatModelGPT4o
+		return openai.ChatModelGPT4_1
 	}
 
-	return openai.ChatModelGPT4oMini
+	return openai.ChatModelGPT4_1Mini
 }
 
 func (ai *OpenAi) GetResponse(history []ChatMessage, quality ModelQuality) (Response, error) {
